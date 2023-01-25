171015e0f515bba1b384ff60980a6ede0bc1f51b
= <rukshanlancer@gmail.com>
2023-01-26 04:31:33 +0800

    settimeout for delay

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    console.log("requested!!!!");
+    const handler = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+    return () => {
+      clearTimeout(handler);
+    };
+
   }, [enteredEmail, enteredPassword]);
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
